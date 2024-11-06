8a14b805aeb194af9f505582c51b3bc64dbe3240
Jared Garcia <114697153+Nllyd@users.noreply.github.com>
2024-11-06 01:47:22 -0500

    Cambios de static en el settings

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -98,10 +98,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/myproject/myapp/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myproject', 'myapp', 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
